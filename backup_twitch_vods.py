fdc8495e19185fc740b74afc5c2c5f8a7451518e
skend <sam.j.kennan@gmail.com>
2022-03-23 00:24:16 +0000

    add auto youtube uploader thingy

--- backup_twitch_vods.py
+++ backup_twitch_vods.py
@@ -1,14 +1,25 @@
-import requests
-import json
-import time
-import youtube_dl
 import argparse
-import log
-import my_secrets
+import json
+import logging
+import time
+import os
 from datetime import datetime
 
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+import requests
+import youtube_dl
+from googleapiclient.http import MediaFileUpload
 
-def is_live(username : str):
+import my_secrets
+
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+
+def get_stream_info(username : str):
     url = f"https://api.twitch.tv/helix/streams?user_login={username}&first=1"
 
     headers = {
@@ -16,39 +27,121 @@ def is_live(username : str):
         'Client-Id': my_secrets.client_id
     }
 
-    log.info(f"checking if {username} is live")
     res = requests.get(url, headers=headers)
-    parsed = json.loads(res.text)
-    if len(parsed['data']) > 0:
-        log.info(f"{username} is live")
+    return json.loads(res.text)
+
+
+def is_live(username: str):
+    stream_data = get_stream_info(username)
+    if len(stream_data['data']) > 0:
+        logging.info(f"{username} is live")
         return True
     else:
-        log.info(f"{username} is offline")
         return False
 
 
-def download_vod(username : str, directory : str):
+def download_vod(username: str, directory: str, stream_data : dict):
+    stream_id = stream_data['id']
     date_str = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     ydl_opts = {
-        'outtmpl': f'{directory}/{date_str}_{username}_%(id)s.%(ext)s',
+        'outtmpl': f'{directory}/{date_str}_{username}_{stream_id}.mp4',
         'quiet': True
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        log.info(f"downloading latest {username} vod")
+        logging.info(f"downloading latest {username} vod")
         ydl.download([f'https://www.twitch.tv/{username}'])
-        log.info(f"finished downloading latest {username} vod")
+        logging.info(f"finished downloading latest {username} vod")
+        return ydl_opts['outtmpl']
+
+
+def get_youtube_category(youtube):
+    country_code = input("Enter 2 letter country code (ISO 3166-1 alpha-2 format): ")
+    if len(country_code) != 2:
+        print("Country code must be 2 letters")
+
+    request = youtube.videoCategories().list(
+        part="snippet",
+        regionCode=country_code.upper()
+    )
+
+    response = request.execute()
+
+    cls()
+    for cat in response['items']:
+        print(f"{cat['id']} - {cat['snippet']['title']}")
+
+    category = input('Enter the number of the category you wish to upload VODs under: ')
+    cls()
+    
+    return category
+
+
+def auth_with_youtube():
+    scopes = ["https://www.googleapis.com/auth/youtube.readonly",
+              "https://www.googleapis.com/auth/youtube.upload"]
+    client_secrets_file = "client_secrets.json"
+
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(client_secrets_file, scopes)
+    credentials = flow.run_console()
+    return googleapiclient.discovery.build("youtube", "v3", credentials=credentials)
+
+
+def upload_to_youtube(youtube, vod_path, category, stream_data):
+    request = youtube.videos().insert(
+        part="snippet,status",
+        body={
+          "snippet": {
+            "title": f"{stream_data['user_name']} - {stream_data['title']} - {stream_data['game_name']}",
+            "description": f"Streamed at {stream_data['started_at']}\nID: {stream_data['id']}",
+            "tags": [
+              stream_data['user_name'],
+              stream_data['game_name'],
+              stream_data['title']
+            ],
+            "categoryId": str(category)
+          },
+          "status": {
+            "privacyStatus": "private"
+          }
+        },
+        media_body=MediaFileUpload(vod_path)
+    )
+    response = request.execute()
+    logging.info(f"YouTube video available at: https://www.youtube.com/watch?v={response['id']}")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog='PROG')
-    parser.add_argument('-u', '--username', type=str, help='Username of Twitch streamer to stalk')
-    parser.add_argument('-p', '--polling_rate', type=int, help='Number of seconds to sleep between stalks')
-    parser.add_argument('-d', '--directory', type=str, help="The directory to the save the vods in")
+    parser = argparse.ArgumentParser(prog='PROG',
+                                     description='Waits for a Twitch streamer to go live, then downloads their VOD. Optionally uploads the VOD straight to YouTube.')
+
+    parser.add_argument('-u', '--username', type=str,
+                        help='Username of Twitch streamer to stalk')
+    parser.add_argument('-p', '--polling_rate', type=int, default=10,
+                        help='Number of seconds to sleep between stalks')
+    parser.add_argument('-d', '--directory', type=str, default='vods',
+                        help="The directory to the save the VODs in")
+    parser.add_argument('-y', '--youtube', action=argparse.BooleanOptionalAction,
+                        default=False, help="Upload the recorded VOD to YouTube once it's completed")
+
     args = parser.parse_args()
 
+    logging.basicConfig(format='%(asctime)s\t%(message)s',
+                        datefmt='%Y-%m-%dT%H:%M:%S', level=logging.INFO)
+
+    youtube = None
+    category = None
+    if args.youtube:
+        youtube = auth_with_youtube()
+        category = get_youtube_category(youtube)
+        print(category)
+
     while (True):
+        logging.info(f"Waiting for {args.username} to turn the stream on")
         if not is_live(args.username):
             time.sleep(args.polling_rate)
             continue
-        download_vod(args.username, args.directory)
-        time.sleep(30)
+        stream_data = get_stream_info(args.username)['data'][0]
+        vod_path = download_vod(args.username, args.directory, stream_data)
+        if args.youtube:
+            upload_to_youtube(youtube, vod_path, category, stream_data)
+        time.sleep(60)
